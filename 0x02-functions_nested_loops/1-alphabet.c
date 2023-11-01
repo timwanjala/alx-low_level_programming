6dd4dc42ae39526d600ff3926001eff2222dbab3
timwanjala <wanjalatimon@gmail.com>
2023-11-01 04:11:05 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabet
+ * @i: alphabet to be printed
+ * Return: 0
+ */
+
+
+
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
