0931bfdfd8c1dd68be6ae12aa0335116bed50a62
timwanjala <wanjalatimon@gmail.com>
2023-11-06 01:55:57 -0800

    print string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string to stdout
+ * @str: The string to be printed
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
